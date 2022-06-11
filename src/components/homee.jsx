d9048122b5aeda190e2afbe1d7ef359acb8f642d
Anandakrishnan66 <ananda2000krishnan@gmail.com>
2022-06-11 08:25:46 +0530

    foot

--- src/components/homee.jsx
+++ src/components/homee.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import '/home/ananda/Desktop/react1/my-app/src/pages/home.css'
-//import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 //import Tooltip from '@mui/material/Tooltip';
-//import Typography from '@mui/material/Typography';
+import Typography from '@mui/material/Typography';
 //import Alert from '@mui/material/Alert';
 //import LinearProgress from '@mui/material/LinearProgress';
 
@@ -13,9 +13,17 @@ import '@fontsource/roboto/700.css';
 
 
 
+
 function Homee() {
+
+  const pic1=require('/home/ananda/Desktop/react1/my-app/src/pages/Other 18.png')
+  const pic2=require('/home/ananda/Desktop/react1/my-app/src/pages/Other 17.png')
+  
+  
+  
+  
   return (
-      <div>
+<div>
     <div style={{
         position:' absolute',
 width:' 10px',
@@ -39,7 +47,7 @@ color: '#FFFFFF'
 
     }}>
       Featured on
-    </div>
+  </div>
 
     <div style={{
         position:'absolute',
@@ -63,13 +71,76 @@ color: '#FFFFFF'
     </div>
 
   
-         <div className='T4'>
+    <div className="analytics">
+         <label  className='ana'>Analytics</label>
 
-         </div>
 
-         <div>
+         <img src={pic1} alt="analytics" style={{
+            width:'500px',
+            height:'500px',
+            position: 'absolute',
+            left: '218px',
+            right: '0%',
+            top: '31%',
+            bottom: '0%'
 
-         </div>
+         }}></img>
+        <Typography className='typ' align='inherit' gutterBottom='true' variant='h3' >Built-in analytics to track your Nfts</Typography>
+         <p className='para'>Use our built-in analytics dashboard to pull valuable insights and monitor the value of your Krypto portfolio over time.</p>
+         
+         <Button id='viewprice'  variant='contained'>View our Pricing</Button>
+          
+
+    </div>
+
+
+    <div className="smart">
+         <label id="applabel">Get Our App</label>
+
+         <p id='Browse'>Browse NFTs from your smartphone</p>
+         <p id='browpara'>Use our built-in analytics dashboard to pull valuable insights and monitor the value of your Krypto portfolio over time.</p>
+          <Button id='viewprice2'variant='contained'>View Price</Button>
+     <img  src={pic2} alt="mobile"  style={{
+          width:'500px',
+          height:'500px',
+         position: 'absolute',
+         left: 'calc(50% - 100px/2)',
+         top: '1655px',
+         bottom: '0%',
+         boxShadow:'9px 9px 9px #8D8DDA',
+         borderRadius:'3cm'
+         
+
+         
+         
+         
+
+     }}></img>
+      
+    
+    </div>
+  
+
+  <div className='eli3'></div>
+
+  <div className="foot">
+         
+           <label id="testi">Testimonials</label>
+           <Typography id="others"gap={'10px'}> Read What Others Have To Say</Typography>
+
+       <div className="boxx1"></div>
+       <div className="boxx2"></div>
+       <div className="boxx3"></div>
+
+
+       <div className='foot1'>
+
+        
+       </div>
+
+
+  </div>
+     
 
 
 
